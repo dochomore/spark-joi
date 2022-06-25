59500a17f05797f5415a1c6c2162313957fcc0f6
Yimesgen Morka <dochomorka@gmail.com>
2022-06-25 14:57:48 +0300

    completed sign in page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
+import React from 'react';
+import { CssVarsProvider, useColorScheme } from '@mui/joy';
+import Sheet from '@mui/joy/Sheet';
+import Typography from '@mui/joy/Typography';
+import TextField from '@mui/joy/TextField';
+import Button from '@mui/joy/Button';
+import Link from '@mui/joy/Link';
+
 import './App.css';
+
+const ModeToggle = () => {
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <Button
+      variant="soft"
+      onClick={() => {
+        if (mode === 'light') {
+          setMode('dark');
+        } else {
+          setMode('light');
+        }
+      }}
+    >{mode === 'light' ? 'Turn dark': 'Turn light'}</Button>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <CssVarsProvider>
+      
+      <Sheet
+        variant="outlined"
+        sx={{
+          maxWidth: 400,
+          mx: 'auto', // margin left & right
+          my: 4, // margin top & botom
+          py: 3, // padding top & bottom
+          px: 2, // padding left & right
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+          borderRadius: 'sm',
+          boxShadow: 'inherit',
+        }}
+      >
+        <Sheet variant='plain' sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+
+        <div>
+          <Typography level="h4" component="h1">
+            <b>Welcome!</b>
+          </Typography>
+          <Typography level="body2">Sign in to continue</Typography>
+        </div>
+        <div>
+        <ModeToggle />
+        </div>
+        </Sheet>
+        <TextField
+          // html input attribute
+          name="email"
+          type="email"
+          placeholder="johndoe@email.com"
+          // pass down to FormLabel as children
+          label="Email"
+        />
+        <TextField
+          name="password"
+          type="password"
+          placeholder="password"
+          label="Password"
+        />
+        <Button
+          sx={{
+            mt: 1, // margin top
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Log in
+        </Button>
+        <Typography
+          endDecorator={<Link href="/sign-up">Sign up</Link>}
+          fontSize="sm"
+          sx={{ alignSelf: 'center' }}
+        >
+          Don't have an account?
+        </Typography>
+      </Sheet>
+    </CssVarsProvider>
   );
 }
 
